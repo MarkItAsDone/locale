354a7711706a2fdaacf96ff46e6e112c718115ff
Hamidreza <hamidreza@markitasdone.com>
2021-09-14 17:10:09 +0430

    update expose file

--- src/main-expose.ts
+++ src/main-expose.ts
@@ -1,3 +1,4 @@
 export { LocaleCache } from './locale-cache/locale-cache';
 export { Locale } from './main/main';
+export { LocaleSide } from './main/main-enum';
 export { NestedLocale } from './nested-locale/nested-locale';
